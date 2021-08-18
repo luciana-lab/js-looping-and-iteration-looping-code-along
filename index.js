a155f07f30c5397580315ffd3a4e441be06cb34a
Luciana Sugeta <lulusugeta@gmail.com>
2021-08-18 14:02:59 -0400

    commit tree 7a700d1907a920ea7965d51a12fade336d2ea37e

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// Couldn't resolve it by myself. Used the solution.
+
+function writeCards( names, event ) {
+    console.log(names)
+    let thankYouCards = []
+    for ( let i = 0; i < names.length; i++ ) {
+        thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` );
+    }
+    return thankYouCards
+}
+
+function countDown( number ) {
+    while ( number > 0 ) {
+        console.log( number );
+        number -= 1;
+    }
+    console.log( number );
+}
